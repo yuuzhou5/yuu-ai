60bfe46628ea40e0d3266e69e2891922a8761ed7
FranciscoZhou5 <fg45874587@proton.me>
2025-01-21 16:19:18 -0300

    Initial commit from Create Next App

---
+++ src/components/ui/chat/chat-input.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+
+interface ChatInputProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const ChatInput = React.forwardRef<HTMLTextAreaElement, ChatInputProps>(
+  ({ className, ...props }, ref) => (
+    <Textarea
+      autoComplete="off"
+      ref={ref}
+      name="message"
+      className={cn(
+        "px-4 py-3 max-h-40 h-full bg-background text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center resize-none",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+ChatInput.displayName = "ChatInput";
+
+export { ChatInput };
