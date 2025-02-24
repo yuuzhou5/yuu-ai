adc870ff10e498f01d11d3a37b4d7e800e4a255b
FranciscoZhou5 <fg45874587@proton.me>
2025-02-24 18:17:56 -0300

    fix: prevent send message to chat with suggested prompts

--- src/components/suggested-actions.tsx
+++ src/components/suggested-actions.tsx
@@ -2,7 +2,10 @@
 
 import { ChatRequestOptions, CreateMessage, Message } from "ai";
 import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
 import { memo, useEffect, useState } from "react";
+
+import { useLoginDialog } from "@/context/login-dialog-context";
 
 import { Button } from "./ui/button";
 
@@ -26,72 +29,75 @@ function drawActions(
   return shuffled.slice(0, quantity);
 }
 
+const suggestedActions = [
+  {
+    title: "Como astronautas na Idade Média",
+    label: "explicariam um foguete?",
+    action: "Astronautas na Idade Média: como explicariam um foguete?",
+  },
+  {
+    title: "Filósofos gregos em um podcast",
+    label: "sobre o que discutiriam?",
+    action: "Filósofos gregos em um podcast: sobre o que discutiriam?",
+  },
+  {
+    title: "Samurais em um concurso de dança",
+    label: "qual estilo eles dominariam?",
+    action: "Samurais em um concurso de dança: qual estilo eles dominariam?",
+  },
+  {
+    title: "Dragões em uma cafeteria",
+    label: "que bebidas eles prefeririam?",
+    action: "Dragões em uma cafeteria: que bebidas eles prefeririam?",
+  },
+  {
+    title: "Gladiadores em um estádio moderno",
+    label: "como seriam seus espetáculos?",
+    action: "Gladiadores em um estádio moderno: como seriam seus espetáculos?",
+  },
+  {
+    title: "Artistas renascentistas usando NFTs",
+    label: "como venderiam suas obras?",
+    action: "Artistas renascentistas usando NFTs: como venderiam suas obras?",
+  },
+  {
+    title: "Vikings em uma viagem espacial",
+    label: "quais planetas explorariam?",
+    action: "Vikings em uma viagem espacial: quais planetas explorariam?",
+  },
+  {
+    title: "Egípcios antigos com emojis",
+    label: "quais símbolos criariam?",
+    action: "Egípcios antigos com emojis: quais símbolos criariam?",
+  },
+  {
+    title: "Napoleão em um aplicativo de encontros",
+    label: "como seria seu perfil?",
+    action: "Napoleão em um aplicativo de encontros: como seria seu perfil?",
+  },
+  {
+    title: "O que os Inventores da Revolução Industrial",
+    label: "fabricariam com um impressoras 3D?",
+    action: "O que os Inventores da Revolução Industrial fabricariam com um impressoras 3D?",
+  },
+  {
+    title: "Quais lugares da China",
+    label: "que você acha que todo mundo deveria ir?",
+    action: "Quais lugares da China que você acha que todo mundo deveria ir?",
+  },
+];
+
+const drawnSuggestedActions = drawActions(suggestedActions, 4);
+
 function PureSuggestedActions({ chatId, append }: SuggestedActionsProps) {
   const [mounted, setMounted] = useState(false);
+
+  const { status } = useSession();
+  const { open } = useLoginDialog();
 
   useEffect(() => {
     setMounted(true);
   }, []);
-
-  const suggestedActions = [
-    {
-      title: "Como astronautas na Idade Média",
-      label: "explicariam um foguete?",
-      action: "Astronautas na Idade Média: como explicariam um foguete?",
-    },
-    {
-      title: "Filósofos gregos em um podcast",
-      label: "sobre o que discutiriam?",
-      action: "Filósofos gregos em um podcast: sobre o que discutiriam?",
-    },
-    {
-      title: "Samurais em um concurso de dança",
-      label: "qual estilo eles dominariam?",
-      action: "Samurais em um concurso de dança: qual estilo eles dominariam?",
-    },
-    {
-      title: "Dragões em uma cafeteria",
-      label: "que bebidas eles prefeririam?",
-      action: "Dragões em uma cafeteria: que bebidas eles prefeririam?",
-    },
-    {
-      title: "Gladiadores em um estádio moderno",
-      label: "como seriam seus espetáculos?",
-      action: "Gladiadores em um estádio moderno: como seriam seus espetáculos?",
-    },
-    {
-      title: "Artistas renascentistas usando NFTs",
-      label: "como venderiam suas obras?",
-      action: "Artistas renascentistas usando NFTs: como venderiam suas obras?",
-    },
-    {
-      title: "Vikings em uma viagem espacial",
-      label: "quais planetas explorariam?",
-      action: "Vikings em uma viagem espacial: quais planetas explorariam?",
-    },
-    {
-      title: "Egípcios antigos com emojis",
-      label: "quais símbolos criariam?",
-      action: "Egípcios antigos com emojis: quais símbolos criariam?",
-    },
-    {
-      title: "Napoleão em um aplicativo de encontros",
-      label: "como seria seu perfil?",
-      action: "Napoleão em um aplicativo de encontros: como seria seu perfil?",
-    },
-    {
-      title: "O que os Inventores da Revolução Industrial",
-      label: "fabricariam com um impressoras 3D?",
-      action: "O que os Inventores da Revolução Industrial fabricariam com um impressoras 3D?",
-    },
-    {
-      title: "Quais lugares da China",
-      label: "que você acha que todo mundo deveria ir?",
-      action: "Quais lugares da China que você acha que todo mundo deveria ir?",
-    },
-  ];
-
-  const drawnSuggestedActions = drawActions(suggestedActions, 4);
 
   return (
     <div className="grid sm:grid-cols-2 gap-2 w-full">
@@ -107,7 +113,14 @@ function PureSuggestedActions({ chatId, append }: SuggestedActionsProps) {
           >
             <Button
               variant="ghost"
-              onClick={async () => {
+              onClick={async (e) => {
+                if (status === "unauthenticated") {
+                  e.preventDefault();
+                  e.stopPropagation();
+
+                  return open();
+                }
+
                 window.history.replaceState({}, "", `/chat/${chatId}`);
 
                 append({
